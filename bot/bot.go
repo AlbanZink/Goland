4b6e91c493918ef917da6ac37d254961d38a891f
Alban Grandmougin-Zink <alban.grandmougin-zink@epitech.eu>
2023-04-28 17:40:11 +0200

    Add: add 2 commands: writemyname and saymyname

--- bot/bot.go
+++ bot/bot.go
@@ -1,8 +1,10 @@
 package bot
 
 import (
+	"encoding/json"
 	"fmt"                       //to print errors
 	"golang-discord-bot/config" //importing our config package which we have created above
+	"os"
 	"time"
 
 	"github.com/bwmarrin/discordgo" //discordgo package from the repo of bwmarrin .
@@ -12,6 +14,10 @@ var (
 	BotId string
 	goBot *discordgo.Session
 )
+
+type User struct {
+	Name string `json:"name"`
+}
 
 func Start() {
 	//creating new bot session
@@ -38,6 +44,14 @@ func Start() {
 			Name:        "time",
 			Description: "Répond à l'intéraction par le temps actuel en utilisant la balise de temps.",
 		},
+		{
+			Name:        "writemyname",
+			Description: "Ecris ton pseudo discord dans un fichier data.json",
+		},
+		{
+			Name:        "saymyname",
+			Description: "Lire le data.json et lister le nom",
+		},
 	})
 	if err != nil {
 		fmt.Println("Error registering slash commands:", err)
@@ -47,6 +61,8 @@ func Start() {
 	// Adding handler function to handle our messages using AddHandler from discordgo package. We will declare messageHandler function later.
 	goBot.AddHandler(messageHandler)
 	goBot.AddHandler(timeCommandhandler)
+	goBot.AddHandler(writeMyNameCommandHandler)
+	goBot.AddHandler(sayMyNameCommandHandler)
 
 	err = goBot.Open()
 	//Error handling
@@ -75,19 +91,89 @@ func timeCommandhandler(s *discordgo.Session, i *discordgo.InteractionCreate) {
 	case "time":
 		// Get the current timestamp in seconds
 		currentTimestamp := time.Now().Unix()
-		err := s.InteractionRespond(
-			i.Interaction,
-			&discordgo.InteractionResponse{
-				Type: discordgo.InteractionResponseChannelMessageWithSource,
-				Data: &discordgo.InteractionResponseData{
-					// Replace EPOCH with the actual timestamp
-					Content: fmt.Sprintf("<t:%d>", currentTimestamp),
-				},
+		nameUser := i.Member.User.Username
+		err := s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+			Type: discordgo.InteractionResponseChannelMessageWithSource,
+			Data: &discordgo.InteractionResponseData{
+				// Replace EPOCH with the actual timestamp
+				Content: fmt.Sprintf("%s, the current time is: <t:%d>", nameUser, currentTimestamp),
 			},
+		},
 		)
 		if err != nil {
 			fmt.Println(err.Error())
 			return
+		}
+	}
+}
+
+func writeMyNameCommandHandler(s *discordgo.Session, i *discordgo.InteractionCreate) {
+	data := i.ApplicationCommandData()
+	switch data.Name {
+	case "writemyname":
+		userName := i.Member.User.Username
+		user := User{Name: userName}
+		users := []User{user}
+		userJSON, err := json.Marshal(users)
+		if err != nil {
+			fmt.Println("Error marshaling JSON:", err)
+			return
+		}
+		file, err := os.Create("userName.json")
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		defer file.Close()
+		_, err = file.Write(userJSON)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		// Send a response back to the user
+		err = s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+			Type: discordgo.InteractionResponseChannelMessageWithSource,
+			Data: &discordgo.InteractionResponseData{
+				Content: "User data written to userName.json file",
+			},
+		})
+		if err != nil {
+			fmt.Println("Error sending response:", err)
+			return
+		}
+	}
+}
+
+func sayMyNameCommandHandler(s *discordgo.Session, i *discordgo.InteractionCreate) {
+	data := i.ApplicationCommandData()
+	switch data.Name {
+	case "saymyname":
+		file, err := os.Open("userName.json")
+		if err != nil {
+			fmt.Println("Error opening file:", err)
+			return
+		}
+		defer file.Close()
+		// Decode the JSON data
+		var users []User
+		err = json.NewDecoder(file).Decode(&users)
+		if err != nil {
+			fmt.Println("Error decoding JSON:", err)
+			return
+		}
+		// Print the users
+		for _, user := range users {
+			userName := user.Name
+			err = s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+				Type: discordgo.InteractionResponseChannelMessageWithSource,
+				Data: &discordgo.InteractionResponseData{
+					Content: userName,
+				},
+			})
+			if err != nil {
+				fmt.Println("Error sending response:", err)
+				return
+			}
 		}
 	}
 }
